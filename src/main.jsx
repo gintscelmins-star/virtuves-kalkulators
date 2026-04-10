16e6c8bdaedbccf8677d9b1a463025cee959ce44
gintscelmins-star <gints.celmins@gmail.com>
2026-04-10 15:57:46 +0300

    feat: add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./i18n";
+import "./index.css";
+import App from "./App";
+const qc = new QueryClient({ defaultOptions: { queries: { staleTime: 5*60*1000 } } });
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={qc}>
+      <BrowserRouter><App /></BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
